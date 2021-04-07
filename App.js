bea1b4a61acc497074843bd04e6f8b16a8d3ace9
sashwot <57551518+sashwot@users.noreply.github.com>
2021-04-07 18:06:17 -0500

    Changed text in view. Still not working.

--- App.js
+++ App.js
@@ -1,12 +1,127 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech'
+
+
+const bodyText = 'I want to go to washroom!'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+      backgroundColor:"#4B0082",
+      flex:0.3,
+      flexDirection:"row",
+      justifyContent:"center",
+      alignItems:"center",
+    }}>
+
+    
+    <TouchableWithoutFeedback onPress={()=>Speech.speak({bodyText}) } >
+      
+      <View
+          style={{
+            backgroundColor:'red',
+            width:90,
+            height:100,
+            top: 165,
+            left:220,
+          }}
+          
+      />
+      <Text>{bodyText}</Text>
+      
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('I am hungry!')}>
+      <View 
+          style={{
+            backgroundColor:'blue',
+            width:90,
+            height:120,
+            top: 165,
+            right:-250,
+          }}
+      />
+    </TouchableWithoutFeedback> 
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('I need your help!')}>
+      <View 
+          style={{
+            backgroundColor:'black',
+            width:90,
+            height:120,
+            top: 165,
+            left:300,
+          }}
+      />
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('I need a power nap!')}>
+      <View 
+          style={{
+            backgroundColor:'purple',
+            width:90,
+            height:120,
+            top: 300,
+            right:49,
+          }}
+      />
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=> Speech.speak('I want to talk with my family member!')}>
+      <View 
+        style={{
+          backgroundColor:'#dc143c',
+          width:90,
+          height:120,
+          top:300,
+          right:20,
+        }}
+      />
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=> Speech.speak('I want to talk with you!')}>
+      <View 
+        style={{
+          backgroundColor:'yellow',
+          width:90,
+          height:120,
+          top:300,
+          right: -30,
+        }}
+      />
+    </TouchableWithoutFeedback> 
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('Please leave me alone!')}>
+      <View 
+        style={{
+          backgroundColor:'orange',
+          width:90,
+          height:120,
+          top:450,
+          right:60,
+        }}
+      />
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('Have a great day!')}>
+      <View 
+        style={{
+          backgroundColor:'green',
+          width:90,
+          height:120,
+          top:450,
+          right:410,
+        }}
+      />
+    </TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={()=>Speech.speak('See you man!')}>
+      <View 
+        style={{
+          backgroundColor:'pink',
+          width:90,
+          height:120,
+          top:450,
+          right:380,
+        }}
+      />
+    </TouchableWithoutFeedback>
       <StatusBar style="auto" />
+    
     </View>
   );
 }
@@ -19,3 +134,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
